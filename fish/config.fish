c652e77a91636536d29a6df6b6d19f152fdd8449
khoidh24 <65653624+khoidh24@users.noreply.github.com>
2024-02-17 17:27:10 +0700

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -31,12 +31,8 @@ set -g GOPATH $HOME/go
 set -gx PATH $GOPATH/bin $PATH
 
 switch (uname)
-    case Darwin
-        source (dirname (status --current-filename))/config-osx.fish
     case Linux
         source (dirname (status --current-filename))/config-linux.fish
-    case '*'
-        source (dirname (status --current-filename))/config-windows.fish
 end
 
 set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
